2ea818c7a135e183d51849ae87d9226ae8615527
Nathan <Nathan@75.102.89.125>
2016-03-21 11:40:34 -0400

    Added the the Course button and panel to create a new internal frame for course.

--- src/IST261DesktopPaneDemo/jfMain.java
+++ src/IST261DesktopPaneDemo/jfMain.java
@@ -117,6 +117,7 @@ public class jfMain extends JFrame {
         jtbMain = new javax.swing.JToolBar();
         jbAddFrame = new javax.swing.JButton();
         jbTile = new javax.swing.JButton(new TileAction(jdpMain));
+        jbCourse = new javax.swing.JButton();
         jmbMain = new javax.swing.JMenuBar();
         jmFile = new javax.swing.JMenu();
         jmiAddFrame = new javax.swing.JMenuItem();
@@ -160,6 +161,13 @@ public class jfMain extends JFrame {
         jbTile.setMinimumSize(new java.awt.Dimension(36, 36));
         jbTile.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         jtbMain.add(jbTile);
+
+        jbCourse.setText("Course");
+        jbCourse.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbCourseActionPerformed(evt);
+            }
+        });
 
         jmFile.setMnemonic('F');
         jmFile.setText("File");
@@ -207,16 +215,21 @@ public class jfMain extends JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jdpMain)
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jtbMain, javax.swing.GroupLayout.PREFERRED_SIZE, 577, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jdpMain)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jbCourse)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jtbMain, javax.swing.GroupLayout.PREFERRED_SIZE, 577, javax.swing.GroupLayout.PREFERRED_SIZE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jtbMain, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jtbMain, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jbCourse))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jdpMain)
                 .addGap(20, 20, 20))
@@ -259,6 +272,13 @@ public class jfMain extends JFrame {
             
         
     }//GEN-LAST:event_jmiTestTablePanelActionPerformed
+
+    private void jbCourseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbCourseActionPerformed
+        JPanel Course = new jpCourse();
+        Course.setName("Course");
+        CreateFrame(Course,Course.getName());
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jbCourseActionPerformed
 
     private void CreateFrame()
     {
@@ -325,6 +345,7 @@ public class jfMain extends JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jbAddFrame;
+    private javax.swing.JButton jbCourse;
     private javax.swing.JButton jbTile;
     private javax.swing.JDesktopPane jdpMain;
     private javax.swing.JMenu jmEdit;
