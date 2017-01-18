43bb7df9c357a3aec3a4116f059493394333f2ef
whb108 <whb108@BKGAIGE209-111.win.psu.edu>
2017-01-18 11:33:01 -0500

    Added Dummy.java as a demo of adding code.  No functionality. - whb108

---
+++ src/IST261DesktopPaneDemo/Dummy.java
@@ -0,0 +1,19 @@
+/*
+No functionality - just to show adding a file to the project - whb108
+*/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package IST261DesktopPaneDemo;
+
+/**
+ * 
+ * @author whb108
+ */
+public class Dummy 
+{
+
+}
