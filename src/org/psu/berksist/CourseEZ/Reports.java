ac4a95842d217e396670203ccdcc9c9d654e0b76
Deathx <Deathx@130.203.82.137>
2017-04-21 12:42:50 -0400

    Merge origin/InitialVersion into InitialVersion

--- src/org/psu/berksist/CourseEZ/Reports.java
+++ src/org/psu/berksist/CourseEZ/Reports.java
@@ -1,37 +1,11 @@
-
-/*
-* Additional Libraries
-*************************************************************
-* antlr-2.7.7
-* antlr-runtime-3.5.2
-* avalon-framework-api-4.3.1
-* avalon-framework-impl-4.3.1
-* commons-codec-1.10
-* commons-io-2.4
-* commons-lang3-3.4
-* commons-logging-1.1.3
-* guava-19.0
-* httpclient-3.5
-* httpcore-4.4.4
-* jackson-annotations-2.7.0
-* jackson-core-2.7.3
-* jackson-databind-2.7.3
-* jaxb-svg11-1.0.2
-* lorem-2.0
-* mbassador-1.2.4.2
-* serializer-2.7.2
-* slf4j-api-1.7.21
-* stringtemplate-3.2.1
-* wmf2svg-0.9.8
-* xalan-2.7.2
-* xmlgraphics-commmons-2.1
-* docx4j-3.3.3
-************************************************************
-*/
 package org.psu.berksist.CourseEZ;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.docx4j.Docx4J;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
@@ -46,15 +20,53 @@ import org.docx4j.wml.STDocProtect;
  */
 public class Reports {
     
+    private Statement st;
+    
+    private Connection dbConnection;
+    
+    
+    
     public void Reports() {}
 
-        public void Syllabus ()throws InvalidFormatException, Docx4JException, FileNotFoundException{
+        public void Syllabus (Connection inConn)throws InvalidFormatException, Docx4JException, FileNotFoundException, SQLException{
                 // creates main document 
-		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.createPackage();
+                dbConnection = inConn;
+                
+                
+                
+                WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.createPackage();
 		MainDocumentPart mdp = wordMLPackage.getMainDocumentPart();
                 // writes into document
-		mdp.addParagraphOfText("Welcome to course EZ");
+		
+                
                 //protects document
+		
+                //st = dbConnection.prepareStatement("SELECT vchrDescription "
+                //            + "FROM SYLLABUS_MASTER;");
+                try {
+      
+
+      st = dbConnection.createStatement();
+      ResultSet rs = st.executeQuery( "SELECT * FROM vSYLLABUS_MASTER;" );
+      
+        
+        String  syllaMain = rs.getString("vchrDescription");
+        int age  = rs.getInt("intNumber");
+        String  address = rs.getString("vchrTitle");
+         
+        mdp.addParagraphOfText(syllaMain);
+        mdp.addParagraphOfText( Integer.toString(age));
+        mdp.addParagraphOfText(address);
+        System.out.println();
+      
+      rs.close();
+      st.close();
+     // c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+                
 		ProtectDocument protection = new ProtectDocument(wordMLPackage);
 		protection.restrictEditing(STDocProtect.READ_ONLY, "ohhhnoooo");
 		
